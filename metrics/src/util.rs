f4bdad8d6bc64ff0104c29535d64436890dbd629
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2022-05-18 21:55:35 +0000

    ✨: watch log file for filesystem changes and terminate faster by using the channel timeout for sleeps.

--- metrics/src/util.rs
+++
@@ -1,8 +0,0 @@
-use std::sync::mpsc::{Receiver, TryRecvError};
-
-pub fn should_stop(rx: &Receiver<()>) -> bool {
-    match rx.try_recv() {
-        Ok(_) | Err(TryRecvError::Disconnected) => true,
-        Err(TryRecvError::Empty) => false
-    }
-}
