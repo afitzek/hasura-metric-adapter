084a1ae3d69c5355225ac4147ef56192e1b38663
Mario Siegenthaler <mario.siegenthaler@linkyard.ch>
2022-05-18 17:14:32 +0200

    ✨: allow clean shutdown

--- metrics/src/collectors/mod.rs
+++ metrics/src/collectors/mod.rs
@@ -1,4 +1,5 @@
-use crate::{Configuration};
+use std::sync::mpsc;
+use crate::{Configuration, util};
 
 mod sql;
 mod health;
@@ -7,7 +8,7 @@ mod scheduled_events;
 mod cron_triggers;
 mod event_triggers;
 
-pub(crate) async fn run_metadata_collector(cfg: &Configuration) -> std::io::Result<()> {
+pub(crate) async fn run_metadata_collector(cfg: &Configuration, rx: &mpsc::Receiver<()>) -> std::io::Result<()> {
     loop {
         tokio::join!(
             health::check_health(cfg),
@@ -16,6 +17,12 @@ pub(crate) async fn run_metadata_collector(cfg: &Configuration) -> std::io::Resu
             cron_triggers::check_cron_triggers(&cfg),
             event_triggers::check_event_triggers(&cfg),
         );
-        tokio::time::sleep(std::time::Duration::from_millis(cfg.collect_interval)).await;
+        for _i in 1..100 { // split up the interval so it stops in a useful amount of time
+            if util::should_stop(rx) {
+                return Ok(());
+            }
+
+            tokio::time::sleep(std::time::Duration::from_millis(cfg.collect_interval / 100)).await;
+        }
     }
 }
